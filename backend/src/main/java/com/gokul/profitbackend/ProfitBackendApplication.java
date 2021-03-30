728ba6e4cdf73bd52dace8fd3420a0620d1e688e
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2021-03-30 15:58:38 +0200

    add backend

---
+++ backend/src/main/java/com/gokul/profitbackend/ProfitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.profitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfitBackendApplication.class, args);
+    }
+
+}
